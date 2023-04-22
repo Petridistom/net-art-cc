0de5a7836947cda8512dc249ed57f6b2003b4727
Tom <petridistom@gmail.com>
2023-04-22 10:50:00 +1000

    replaced files

--- public/sketch.js
+++ public/sketch.js
@@ -2,9 +2,20 @@ document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
 function setup () {
-   createCanvas (innerWidth, innerHeight)
+   createCanvas (innerWidth, innerHeight);
+   background (0);
+   noStroke();
 }
 
 function  draw () {
-   background (`turquoise`)
+fill(random(180,255), random(200,255), random(230, 255));
+if (mouseIsPressed) {
+   circle(mouseX, mouseY, 120);
+   // console.log(`your mum is ${mouseX} kilos fat`);
+}
+}
+
+function mousePressed() {
+   background(random(180, 255), random(200, 255), random(180,255));
+
 }
